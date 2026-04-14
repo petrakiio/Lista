40a9b25ebe682fc421fc89b452d464d36425cc3a
petrakiio <eup0022@gmail.com>
2026-04-14 15:57:08 -0500

    Fazendo js

--- Aulas/projetinhos/fakezap/script.js
+++ Aulas/projetinhos/fakezap/script.js
@@ -1,6 +1,9 @@
+const { createElement } = require("react");
+
 let input;
 const btn = document.getElementById('btn');
 
 function Create(msg){
-    
+    const div = createElement('div');
+    div.classList.add('message sent');
 }
