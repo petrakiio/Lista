02347ab1f580faf9ce7cf31b0acba992bc0c725a
petrakiio <eup0022@gmail.com>
2026-04-13 19:39:51 -0500

    Button criado

---
+++ Aulas/projetinhos/gerador/script.js
@@ -0,0 +1,5 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const btn = document.createElement('button'); 
+    btn.textContent = "Clique aqui";
+    document.body.appendChild(btn);
+});
