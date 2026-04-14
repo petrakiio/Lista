cd6679363e52fddb75c816874c3ef06853718815
petrakiio <eup0022@gmail.com>
2026-04-13 19:59:57 -0500

    Logica feita

--- Aulas/projetinhos/gerador/script.js
+++ Aulas/projetinhos/gerador/script.js
@@ -1,10 +1,18 @@
+import { gerarSenha } from "./gerador";
+
+function Layout(){
+    const password = gerarSenha();
+    const p = document.createElement('p')
+    p.textContent=`You password:${password}`;
+}
+
 function Main(){
     const btn = document.createElement('button'); 
     btn.textContent = "Click Here";
     document.body.appendChild(btn);
 
     document.querySelector('button').addEventListener('click',()=>{
-    console.log('Button clicked!')
+        
     });
 }
 
