049fd8ba4cae76891c26e79c678462560d5c9a8a
petrakiio <eup0022@gmail.com>
2026-04-14 16:00:59 -0500

    Script feito

--- Aulas/projetinhos/fakezap/script.js
+++ Aulas/projetinhos/fakezap/script.js
@@ -1,9 +1,19 @@
-const { createElement } = require("react");
-
 let input;
 const btn = document.getElementById('btn');
+
+function GetValue(){
+    input = document.getElementById('msg').value;
+    return {msg:input};
+};
 
 function Create(msg){
     const div = createElement('div');
     div.classList.add('message sent');
-}
+    div.textContent=msg;
+};
+
+function Main(){
+    Create(GetValue());
+};
+
+btn.addEventListener('click',Main);
