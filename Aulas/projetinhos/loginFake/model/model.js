b51a40f0311fe81123291f71c3baf72bd3c2019d
petrakiio <eup0022@gmail.com>
2026-04-18 16:28:45 -0500

    Model feito

--- Aulas/projetinhos/loginFake/model/model.js
+++ Aulas/projetinhos/loginFake/model/model.js
@@ -1,10 +1,29 @@
-class User{
-    constructor(name,age,email){
+class User {
+    constructor(name, age, email) {
+        this.id = Date.now(); // id único
         this.name = name;
         this.age = age;
         this.email = email;
     }
-    Insert(user){
-        localStorage.setItem("user",JSON.stringify(user));
+
+    save() {
+        const users = JSON.parse(localStorage.getItem("users")) || [];
+        users.push(this);
+        localStorage.setItem("users", JSON.stringify(users));
+    }
+
+    static getAll() {
+        return JSON.parse(localStorage.getItem("users")) || [];
+    }
+
+    static getById(id) {
+        const users = JSON.parse(localStorage.getItem("users")) || [];
+        return users.find(user => user.id === id);
+    }
+
+    static delete(id) {
+        const users = JSON.parse(localStorage.getItem("users")) || [];
+        const filtered = users.filter(user => user.id !== id);
+        localStorage.setItem("users", JSON.stringify(filtered));
     }
 }
