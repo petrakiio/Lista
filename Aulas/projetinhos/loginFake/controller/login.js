47c52eb8212d4a2c32b0479012ec8ebfad8791f5
petrakiio <eup0022@gmail.com>
2026-04-18 22:01:43 -0500

    Fazendo busca de users

--- Aulas/projetinhos/loginFake/controller/login.js
+++ Aulas/projetinhos/loginFake/controller/login.js
@@ -2,10 +2,21 @@ import { User } from "../model/model";
 
 function GetValues(){
     return {
-        nome:document.getElementById('')
+        name:document.getElementById('name').value,
+        email:document.getElementById('email').value
     }
 }
 
+function GetUser(dados,users){
+    for(const user of users){
+        if (dados.name === user.name && dados.email === user.email){
+            return user;
+        }
+    }
+    return false;
+}
+
 export function Login(){
-    const users = User.getAll();
+    const result = GetUser(GetValues,User.getAll);
+    
 }
