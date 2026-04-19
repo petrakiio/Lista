6a80db9772c7ab410d78de8f73ef35eb20e12a91
petrakiio <eup0022@gmail.com>
2026-04-18 22:19:07 -0500

    Fazendo cadastro

--- Aulas/projetinhos/loginFake/view/scripts/view_cadastro.js
+++ Aulas/projetinhos/loginFake/view/scripts/view_cadastro.js
@@ -1,5 +1,29 @@
 import { Insert } from "../../controller/cadastro";
 
+function GetValeus(){
+    return {
+        name:document.getElementById('name').value,
+        age:document.getElementById('age').value,
+        email:document.getElementById('email').value
+    }
+}
+
+function sleep() {
+    setTimeout(() => {
+        console.log('Esperando')
+    }, 10000);
+}
+
+function Create(result){
+    if (result === false){
+        const h2 = document.createElement('h2');
+        h2.textContent='Erro ao inserir os dados!';
+        h2.innerHTML='';
+    }
+}
+
 function Main(){
-    const result = Insert();
+    const values = GetValeus();
+    const result = Insert(values.name,values.age,values.email);
+
 }
