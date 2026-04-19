26cd91b738bfc9a990e611e8c6b7ff52f2c0c6ce
petrakiio <eup0022@gmail.com>
2026-04-19 09:09:46 -0500

    Login feito

--- Aulas/projetinhos/loginFake/view/scripts/view_login.js
+++ Aulas/projetinhos/loginFake/view/scripts/view_login.js
@@ -7,6 +7,26 @@ function GetValeus(){
     }
 }
 
+function sleep() {
+    setTimeout(() => {
+        console.log('Esperando')
+    }, 10000);
+}
+
+
+function Create(result){
+    if (result === false){
+        const h2 = document.createElement('h2');
+        h2.textContent='Usuario não encontrado!';
+        sleep();
+        h2.innerHTML='';
+    }else{
+        window.location.href='./index.html'
+    }
+}
+
 function Main(){
-    
+    const values = GetValeus();
+    const result = Login(values.name,values.email);
+    Create(result);
 }
