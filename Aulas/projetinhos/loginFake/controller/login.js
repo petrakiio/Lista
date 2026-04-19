17c8516848de2ae5522edbd0f0b3444c1ccc1a75
petrakiio <eup0022@gmail.com>
2026-04-18 21:54:17 -0500

    Fazendo login

---
+++ Aulas/projetinhos/loginFake/controller/login.js
@@ -0,0 +1,11 @@
+import { User } from "../model/model";
+
+function GetValues(){
+    return {
+        nome:document.getElementById('')
+    }
+}
+
+export function Login(){
+    const users = User.getAll();
+}
