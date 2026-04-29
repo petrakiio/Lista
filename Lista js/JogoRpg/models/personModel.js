54b8f664fb62dd6083c80c925f78eab3c82edde7
petrakiio <eup0022@gmail.com>
2026-04-28 19:47:47 -0500

    Metodo de dano feito

---
+++ Lista js/JogoRpg/models/personModel.js
@@ -0,0 +1,18 @@
+export class Persona{
+    constructor(name,damage,life){
+        this.name = name;
+        this.damage = damage;
+        this.life = life;
+        this.inventory = [];
+    }
+    ReciveDamage(dam,dado){
+        switch(dado){
+            case 0:
+                return `${this.name} Desviou do golpe`;
+            case 1:
+                return `${this.name} recebeu ${this.life -= dam} de dano!`;
+            case 2:
+                return `${this.name} recebeu um critial damage de ${this.life -= (dam * 2)}`;
+        }
+    }
+}
