694ffa91ef5c0d7fbaf0e1277260288744c06bc8
petrakiio <eup0022@gmail.com>
2026-05-03 17:26:40 -0500

    Fazendo configuração de personaguem

---
+++ Lista js/JogoRpg/controller/configCharacter.js
@@ -0,0 +1,20 @@
+import { Persona } from "../models/personModel"
+
+const prompt = require('prompt-sync')();
+
+function Easteregg(nome){
+    if(nome === 'petrakio'){
+        console.log('personaguem criado!');
+        return new Persona('petrakiio',10000,10000);
+    }
+    return;
+}
+
+export function CreatePlayer(){
+    console.log('='.repeat(30));
+    console.log('Vamos criar seu personagem');
+    console.log('='.repeat(30));
+    const name = prompt('Seu Nome:');
+    Easteregg(name);
+    
+}
