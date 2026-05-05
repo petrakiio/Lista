dbcb9f4fbbc3bcc1679c80993e989c67f2604eb5
petrakiio <eup0022@gmail.com>
2026-05-04 19:18:27 -0500

    Fazendo controller

---
+++ Lista js/Cad/view/src/cadastro.js
@@ -0,0 +1,30 @@
+
+
+const div = document.querySelector('.content');
+
+function getvalue(){
+    return {
+        name:document.getElementById('name').value,
+        email:document.getElementById('email').value,
+        password:document.getElementById('password').value
+    }
+}
+
+function validationSaida(v){
+    if(v){
+        const p = document.createElement('p');
+        p.className='pass';
+        p.textContent = 'Conta criada!';
+        div.appendChild(p)
+    }else{
+        const p = document.createElement('p');
+        p.className='err';
+        p.textContent = 'Erro ao criar conta';
+        div.appendChild(p)
+    }
+}
+
+function main(){
+    const values = getvalue();
+    
+}
